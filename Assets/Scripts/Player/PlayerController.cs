eaabfb868f68d860d32329d72dd8dd0b61df9f93
OrangeChef <redyellowchef@gmail.com>
2022-04-12 17:08:37 -0400

    Added advanced player controller and interactables

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,84 +9,237 @@ public class PlayerController : MonoBehaviour
     [Header("Input")]
     [SerializeField] private string horizontalAxis = "Horizontal";
     [SerializeField] private string verticalAxis = "Vertical";
+
+    [Space]
     [SerializeField] private string jumpButton = "Jump";
+    [SerializeField] private string runButton = "Run";
+    [SerializeField] private string crouchButton = "Crouch";
 
     [Header("Movement")]
-    [SerializeField] private float walkSpeed = 5.8f;
-    [SerializeField] private float groundAccel = 0.1f;
-    [SerializeField] private float airAccel = 0.02f;
+    [SerializeField] private float walkSpeed = 5.5f;
+    [SerializeField] private float runSpeed = 8.5f;
+    [SerializeField] private float crouchSpeed = 2.5f;
+    [SerializeField] private float movementAccelOnGround = 0.1f;
+    [SerializeField] private float movementAccelOffGround = 0.005f;
 
-    [Header("Jumping")]
-    [SerializeField] private float jumpHeight = 1.8f;
+    [Space]
+    [SerializeField] private bool canJumpWhenCrouched = false;
+    [SerializeField] private float jumpHeight = 1.5f;
     [SerializeField] private float coyoteTime = 0.12f;
-    [SerializeField] private float bufferTime = 0.1f;
+    [SerializeField] private float bufferTime = 0.12f;
 
-    [Header("Gravity")]
+    [Space]
     [SerializeField] private float gravityScale = 3.0f;
-    [SerializeField] private float terminalVelocity = -120.0f;
+    [SerializeField] private float terminalVelocity = -80.0f;
+
+    [Header("Tweening")]
+    [SerializeField] private float standingHeight = 2.0f;
+    [SerializeField] private float crouchedHeight = 1.0f;
+
+    [Space]
+    [SerializeField] private float crouchTime = 0.3f;
+    [SerializeField] private float uncrouchTime = 0.3f;
+
+    [Space]
+    [SerializeField] private LeanTweenType crouchType = LeanTweenType.easeOutQuart;
+    [SerializeField] private LeanTweenType uncrouchType = LeanTweenType.easeOutQuart;
+
+    [Header("Permissions")]
+    public bool canWalk = true;
+    public bool canRun = true;
+    public bool canCrouch = true;
+    public bool canJump = true;
 
     private CharacterController controller;
-    private Vector3 targetMovement;
-    private Vector3 actualMovement;
-    private Vector3 velocity;
-    private float acceleration;
-    private float groundTime;
-    private float jumpTime;
-    private bool onGround;
+    private CameraController camController;
+    private Vector3 targetDirection;
+    private Vector3 currentDirection;
+    private Vector3 currentVelocity;
+    private float lastTimeOnGround;
+    private float jumpPressedTime;
+    private float controllerGravity;
+    private float controllerSpeed;
+    private float currentAcceleration;
+    private bool isRunning;
+    private bool isCrouching;
+    private bool isOnGround;
 
     void Start()
     {
-        enabled = TryGetComponent(out controller);
+        controller = GetComponent<CharacterController>();
+        camController = GetComponentInChildren<CameraController>();
+
+        if (!controller || !camController)
+        {
+            enabled = false;
+            return;
+        }
+
+        controllerSpeed = walkSpeed;
+
+        canWalk = true;
+        canRun = true;
+        canCrouch = true;
+        canJump = true;
     }
 
     void Update()
     {
-        onGround = controller.isGrounded;
-        acceleration = onGround ? groundAccel : airAccel;
+        isOnGround = controller.isGrounded;
 
-        float x = Input.GetAxisRaw(horizontalAxis);
-        float z = Input.GetAxisRaw(verticalAxis);
+        currentAcceleration = isOnGround ? movementAccelOnGround : movementAccelOffGround;
+        targetDirection.x = Mathf.MoveTowards(targetDirection.x, Input.GetAxisRaw(horizontalAxis), currentAcceleration);
+        targetDirection.z = Mathf.MoveTowards(targetDirection.z, Input.GetAxisRaw(verticalAxis), currentAcceleration);
 
-        targetMovement = transform.right * x + transform.forward * z;
-        actualMovement = Vector3.MoveTowards(actualMovement, targetMovement, acceleration);
-        actualMovement = Vector3.ClampMagnitude(actualMovement, 1.0f);
+        currentDirection = transform.right * targetDirection.x + transform.forward * targetDirection.z;
+        currentDirection = Vector3.ClampMagnitude(currentDirection, 1.0f);
 
-        if (onGround)
+        controllerGravity = Physics.gravity.y * gravityScale;
+
+        if (isOnGround)
         {
-            velocity.y = -1.0f;
+            currentVelocity.y = -2.0f;
             controller.slopeLimit = 45.0f;
-            controller.stepOffset = 0.3f;
-            groundTime = coyoteTime;
+            controller.stepOffset = 0.5f;
+
+            lastTimeOnGround = coyoteTime;
         }
         else
         {
-            if (velocity.y > terminalVelocity)
-                velocity.y += Physics.gravity.y * gravityScale * Time.deltaTime;
-
             controller.slopeLimit = 90.0f;
             controller.stepOffset = 0.0f;
-            groundTime -= Time.deltaTime;
+
+            currentVelocity.y += controllerGravity * Time.deltaTime;
+            currentVelocity.y = Mathf.Clamp(currentVelocity.y, terminalVelocity, -terminalVelocity);
+
+            lastTimeOnGround -= Time.deltaTime;
+        }
+
+        if (Physics.Raycast(transform.position, transform.up, (controller.height / 2.0f) + 0.1f) && currentVelocity.y > 0.0f)
+        {
+            currentVelocity.y = -2.0f;
         }
 
         if (Input.GetButtonDown(jumpButton))
-            jumpTime = bufferTime;
-        else
-            jumpTime -= Time.deltaTime;
-
-        if (groundTime > 0.0f && jumpTime > 0.0f)
         {
-            groundTime = 0.0f;
-            jumpTime = 0.0f;
-            velocity.y = Mathf.Sqrt(jumpHeight * Mathf.Abs(Physics.gravity.y * gravityScale * 2.0f));
+            jumpPressedTime = bufferTime;
+        }
+        else
+        {
+            jumpPressedTime -= Time.deltaTime;
         }
 
-        if (Physics.Raycast(transform.position, Vector3.up, 1.1f))
-            velocity.y = -1.0f;
+        if (Input.GetButton(runButton))
+        {
+            StartRunning();
+        }
 
-        if (actualMovement.magnitude > 0.0f)
-            controller.Move(actualMovement * walkSpeed * Time.deltaTime);
+        if (Input.GetButtonUp(runButton))
+        {
+            StopRunning();
+        }
 
-        if (!onGround || (onGround && velocity.y != -1.0f))
-            controller.Move(velocity * Time.deltaTime);
+        if (Input.GetButtonDown(crouchButton))
+        {
+            StartCrouching();
+        }
+
+        if (Input.GetButtonUp(crouchButton))
+        {
+            StopCrouching();
+        }
+
+        if (jumpPressedTime > 0.0f && lastTimeOnGround > 0.0f)
+        {
+            Jump();
+        }
+
+        if (currentDirection.magnitude > 0.0f && canWalk)
+        {
+            controller.Move(currentDirection * controllerSpeed * Time.deltaTime);
+        }
+
+        controller.Move(currentVelocity * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (isCrouching && !canJumpWhenCrouched)
+        {
+            StopCrouching();
+            return;
+        }
+
+        if (canJump)
+        {
+            lastTimeOnGround = 0.0f;
+            jumpPressedTime = 0.0f;
+            currentVelocity.y = Mathf.Sqrt(jumpHeight * Mathf.Abs(controllerGravity * 2.0f));
+        }
+    }
+
+    void StartRunning()
+    {
+        if (isRunning || isCrouching || !canRun ||
+            currentDirection.magnitude == 0.0f || targetDirection.z <= 0.0f)
+        {
+            return;
+        }
+
+        isRunning = true;
+        controllerSpeed = runSpeed;
+    }
+
+    void StopRunning()
+    {
+        if (isRunning)
+        {
+            isRunning = false;
+            controllerSpeed = walkSpeed;
+        }
+    }
+
+    void StartCrouching()
+    {
+        if (!canCrouch)
+        {
+            return;
+        }
+
+        if (isRunning)
+        {
+            StopRunning();
+        }
+
+        isCrouching = true;
+        controllerSpeed = crouchSpeed;
+
+        LeanTween.value(gameObject, controller.height, crouchedHeight, crouchTime)
+            .setEase(crouchType).setOnUpdate((float height) =>
+            {
+                camController.transform.localPosition = new Vector3(0.0f, height * 0.4f, 0.0f);
+                controller.height = height;
+            });
+    }
+
+    void StopCrouching()
+    {
+        if (Physics.Raycast(transform.position, transform.up, controller.height * 1.5f))
+        {
+            return;
+        }
+
+        if (isCrouching)
+        {
+            isCrouching = false;
+            controllerSpeed = walkSpeed;
+
+            LeanTween.value(gameObject, controller.height, standingHeight, uncrouchTime)
+                .setEase(uncrouchType).setOnUpdate((float height) =>
+                {
+                    camController.transform.localPosition = new Vector3(0.0f, height * 0.4f, 0.0f);
+                    controller.height = height;
+                });
+        }
     }
 }

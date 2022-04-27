57f34a6f892141c34c3720c843c9f04ec766b621
Guillermo Samaruga <guillesama1@gmail.com>
2022-04-27 19:32:09 -0300

    hold mechanic

---
+++ Assets/Scripts/Interactables/HoldableInteractable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldableInteractable : InteractableObject
+{
+    public void Activate(GameObject holdPoint)
+    {
+        base.Activate();
+    }
+}
--- Assets/Scripts/Player/InteractionController.cs
+++ Assets/Scripts/Player/InteractionController.cs
@@ -7,10 +7,18 @@ public class InteractionController : MonoBehaviour
 
     [Header("Input")]
     [SerializeField] private string interactButton = "Interact";
+    [SerializeField] private string holdButton = "Hold";
 
     [Header("Interactions")]
     [SerializeField] private float interactionDistance = 3.0f;
     [SerializeField] private LayerMask interactableMask;
+    [SerializeField] private LayerMask holdableMask;
+    [SerializeField] private Transform holdPositionTransform;
+    [SerializeField] private float holdCenteringForce = 5f;
+    private GameObject heldObject;
+    private Coroutine CenterObjectCoroutine;
+
+    private bool holding = false;
 
     void Update()
     {
@@ -21,6 +29,66 @@ public class InteractionController : MonoBehaviour
             {
                 result.transform.gameObject.SendMessage("Activate");
             }
+        }
+
+        if (Input.GetButtonDown(holdButton) && !holding)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance, holdableMask))
+            {
+                heldObject = hit.transform.gameObject;
+                HoldObject();
+            }
+        }
+        if (Input.GetButtonUp(holdButton) && holding)
+        {
+            DropObject();
+        }
+    }
+
+    private void HoldObject ()
+    {
+        holding = true;
+        heldObject.GetComponent<Rigidbody>().useGravity = false;
+        heldObject.GetComponent<Rigidbody>().drag = 10f;
+        heldObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        heldObject.transform.position = holdPositionTransform.position;
+        CenterObjectCoroutine = StartCoroutine(CenterHeldObject());
+    }
+
+    private void DropObject ()
+    {
+        holding = false;
+        StopCoroutine(CenterObjectCoroutine);
+        if (heldObject != null)
+        {
+            heldObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            heldObject.GetComponent<Rigidbody>().useGravity = true;
+            heldObject.GetComponent<Rigidbody>().drag = 0f;
+            heldObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    private IEnumerator CenterHeldObject()
+    {
+        Rigidbody rgdbdy = heldObject.GetComponent<Rigidbody>();
+        while (holding)
+        {
+            float dist = Vector3.Distance(rgdbdy.position, holdPositionTransform.position);
+            if (dist > 2f)
+            {
+                DropObject();
+            }
+            else if (dist > 0.01f)
+            {
+                rgdbdy.MoveRotation(holdPositionTransform.rotation);
+                rgdbdy.AddForce( (holdPositionTransform.position - rgdbdy.position) * holdCenteringForce, ForceMode.VelocityChange);
+            }
+            else
+            {
+                rgdbdy.MovePosition(holdPositionTransform.position);
+            }
+            yield return new WaitForFixedUpdate();
         }
     }
 }
